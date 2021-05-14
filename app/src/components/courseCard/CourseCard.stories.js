10b9dbb02363ba6197887f7d76acf76639d81093
Punit Jajodia <punitjajodia@Punits-MacBook-Pro.local>
2021-05-14 18:31:31 +0545

    Configured react styleguidist

--- app/src/components/courseCard/CourseCard.stories.js
+++
@@ -1,9 +0,0 @@
-import CourseCard from "./Index";
-
-export default {
-  title: "Course Card",
-  component: CourseCard,
-};
-
-const course = (args) => <CourseCard />;
-export const Card = course.bind({});
