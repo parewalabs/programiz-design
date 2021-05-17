c79b49aeef2e63c67c6100ccd064587ece6586f9
Sayush Lal Shrestha <lalsayush@gmail.com>
2021-05-17 16:43:28 +0545

    Challenge Card Card

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       width: {
         350: "350px",
         160: "160px",
+        37: "37px",
       },
       height: {
         236: "236px",
@@ -15,7 +16,7 @@ module.exports = {
       textColor: {
         "primary-blue": "#25265E",
         link: "#0556F3",
-        caption: "#25265E",
+        caption: "#25265e",
       },
       fontSize: {
         22: "22px",
@@ -27,6 +28,8 @@ module.exports = {
         main: "#0556F3",
         secondary: "#6501E5",
         tertiary: "#00A3FF",
+        warning: "#F79522",
+        seperator: "#D3DCE6",
       },
       fontFamily: {
         sans: ["Euclid Circular A", "Helvetica", "Arial", "sans-serif"],
