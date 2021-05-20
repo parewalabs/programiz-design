c7481cd5b556d8145ac904d1c112725359f9b598
Sayush Lal Shrestha <lalsayush@gmail.com>
2021-05-20 17:38:19 +0545

    example Card

---
+++ app/src/components/ExamplesCard/Index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import vector from "../../images/Vector.png";
+
+const ExamplesCard = (props) => {
+  return (
+    <div className="w-255 h-132 rounded border-2 text-primary-blue focus:ring-2 hover:border-tertiary relative">
+      <div className="px-6 pt-6">
+        <h4 className="text-lg text-primary-blue">{props.name}</h4>
+      </div>
+      <img src={vector} className="absolute -top-0 -right-2" />
+    </div>
+  );
+};
+
+export default ExamplesCard;
