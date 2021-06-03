eb3269ca992588c7f3e62ec5cc5917952df8fa7b
Sayush Lal Shrestha <lalsayush@gmail.com>
2021-06-03 17:24:42 +0545

    import css file

--- app/src/lib/index.js
+++ app/src/lib/index.js
@@ -1,4 +1,5 @@
 import AllLanguagesCard from "./components/LanguageCategoryCard/AllLanguagesCard";
 import LanguageCategoryCard from "./components/LanguageCategoryCard/Index";
+import "./index.css";
 
 export { AllLanguagesCard, LanguageCategoryCard };
