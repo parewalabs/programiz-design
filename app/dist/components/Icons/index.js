e11d839a87b7177091de07bbe5e889c45a18c878
Sayush Lal Shrestha <lalsayush@gmail.com>
2021-06-03 20:18:34 +0545

    add icon

--- app/dist/components/Icons/index.js
+++ app/dist/components/Icons/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import dashboard from "../../../icons/dashboard.svg";
-import course from "../../../icons/course.svg";
-import search from "../../../icons/searchIcon.svg";
-import bookmark from "../../../icons/bookmark.svg";
-import downarrow from "../../../icons/downarrow.svg";
-import uparrow from "../../../icons/uparrow.svg";
-import userPlus from "../../../icons/userPlus.svg";
-import edit from "../../../icons/edit.svg";
-import compiler from "../../../icons/compiler.svg";
-import book from "../../../icons/book.svg";
-import rightarrow from "../../../icons/rightarrow.svg";
-import challengesIcon from "../../../icons/challengesIcon.svg";
-import newTag from "../../../icons/newTag.svg"; //language Icons
+import course from "../../icons/course.svg";
+import search from "../../icons/searchIcon.svg";
+import bookmark from "../../icons/bookmark.svg";
+import downarrow from "../../icons/downarrow.svg";
+import uparrow from "../../icons/uparrow.svg";
+import userPlus from "../../icons/userPlus.svg";
+import edit from "../../icons/edit.svg";
+import compiler from "../../icons/compiler.svg";
+import book from "../../icons/book.svg";
+import rightarrow from "../../icons/rightarrow.svg";
+import challengesIcon from "../../icons/challengesIcon.svg";
+import newTag from "../../icons/newTag.svg"; //language Icons
 
-import python from "../../../icons/python.svg";
-import c from "../../../icons/c.svg";
-import cpp from "../../../icons/cpp.svg";
-import java from "../../../icons/java.svg";
-import js from "../../../icons/js.svg";
-import das from "../../../icons/dsa.svg";
-import csharp from "../../../icons/csharp.svg";
-import kotlin from "../../../icons/kotlin.svg";
-import swift from "../../../icons/swift.svg";
+import python from "../../icons/python.svg";
+import c from "../../icons/c.svg";
+import cpp from "../../icons/cpp.svg";
+import java from "../../icons/java.svg";
+import js from "../../icons/js.svg";
+import das from "../../icons/dsa.svg";
+import csharp from "../../icons/csharp.svg";
+import kotlin from "../../icons/kotlin.svg";
+import swift from "../../icons/swift.svg";
 
 const Icons = props => {
   const selectIcon = iconName => {
