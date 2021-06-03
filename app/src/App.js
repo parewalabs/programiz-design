4f0bddf769ae60ac12afeb14db959f96a1b557f1
Sayush Lal Shrestha <lalsayush@gmail.com>
2021-06-03 21:50:03 +0545

    edit font path

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,7 @@
-import { AllLanguagesCard, LanguageCategoryCard } from "./dist/index";
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl">Programiz design</h1>
-      <AllLanguagesCard />
-      <LanguageCategoryCard languageCode="cpp" language="C++" />
     </div>
   );
 }
