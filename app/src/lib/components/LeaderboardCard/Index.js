487bdd24522323116ab4e3bb1c9e823097dbd1f0
Sayush Lal Shrestha <lalsayush@gmail.com>
2021-06-04 16:35:19 +0545

    edit leaderbord props

--- app/src/lib/components/LeaderboardCard/Index.js
+++ app/src/lib/components/LeaderboardCard/Index.js
@@ -1,91 +1,102 @@
 import "../../index.css";
 import React from "react";
-import avatar from "../../images/avatar.png";
-import avatar1 from "../../images/avatar1.png";
 
 const LeaderboardCard = (props) => {
   return (
     <div className="w-350 h-auto rounded-md border text-primary-blue focus:ring-2 hover:border-main">
-      <div>
-        <h1 className="text-2xl leading-9 font-semibold text-primary-blue text-center pt-8">
-          Leaderboard
-        </h1>
-        {props.topUsers.map((topUser, index) => {
-          return (
-            <div key={index} className="w-full px-6 pt-4 flex">
-              <div className="w-14 text-sm inline-block flex items-center">
-                {topUser.rank}.
+      {/*<div>*/}
+      <h1 className="text-2xl leading-9 font-semibold text-primary-blue text-center pt-8">
+        Leaderboard
+      </h1>
+      {props.topUsers.map((topUser) => {
+        return (
+          <div
+            key={topUser.leaderboardUser.userId}
+            className="w-full px-6 pt-4 flex"
+          >
+            <div className="w-14 text-sm flex items-center">
+              {topUser.rank}.
+            </div>
+            <div className="px-2 w-full h-full flex">
+              <div className="w-9 h-9 rounded-full bg-blue-400">
+                {topUser.leaderboardUser.profilePic !== "" && (
+                  <img
+                    src={topUser.leaderboardUser.profilePic}
+                    className="w-9 h-9 rounded-full"
+                    alt={topUser.leaderboardUser.fullName}
+                  />
+                )}
               </div>
-              <div className="px-2 w-full inline-block flex">
-                <img
-                  src={avatar}
-                  className="w-9 h-9 rounded-full"
-                  alt={topUser.name}
-                />
-                <div className="p-2 text-base font-normal inline-block align-middle">
-                  {topUser.name}
+              <div className="pl-2 text-base font-normal flex items-center">
+                {topUser.leaderboardUser.fullName}
+              </div>
+            </div>
+            <div className="pt-2">
+              <div className="h-5 w-auto px-2 inline-block border border-seperator rounded-xl flex items-center justify-center">
+                <span className="align-middle text-sm font-bold text-warning">
+                  {topUser.score}
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <div className="mt-2 pb-10">
+        {props.realtiveUsers.map((realtiveUser) => {
+          let active = false;
+          if (realtiveUser.leaderboardUser.userId === realtiveUser.loggedInId) {
+            active = true;
+          }
+          return (
+            <div
+              key={realtiveUser.leaderboardUser.userId}
+              className={`px-5 py-1 w-full flex ${
+                active === true
+                  ? "border-l-2 border-secondary first:bg-compiler-base"
+                  : ""
+              }`}
+            >
+              <div className="w-14 text-sm flex items-center">
+                {realtiveUser.rank}.
+              </div>
+              <div className="px-2 w-full flex">
+                <div
+                  className={`items-center ${
+                    active === true
+                      ? "p-0.5 rounded-full border border-secondary"
+                      : ""
+                  }`}
+                >
+                  <div className="w-9 h-9 rounded-full bg-blue-400">
+                    {realtiveUser.leaderboardUser.profilePic !== "" && (
+                      <img
+                        src={realtiveUser.leaderboardUser.profilePic}
+                        className="w-9 h-9 rounded-full"
+                        alt={realtiveUser.leaderboardUser.fullName}
+                      />
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={`pl-2 text-base font-normal flex items-center ${
+                    active === true ? "font-semibold" : ""
+                  }`}
+                >
+                  {realtiveUser.leaderboardUser.fullName}
                 </div>
               </div>
               <div className="pt-2">
-                <div className="h-5 w-37 inline-block border border-seperator rounded-xl flex items-center justify-center">
+                <div className="h-5 w-auto px-2 border border-seperator rounded-xl flex items-center justify-center">
                   <span className="align-middle text-sm font-bold text-warning">
-                    {topUser.score}
+                    {realtiveUser.score}
                   </span>
                 </div>
               </div>
             </div>
           );
         })}
-        <div className="mt-2 pb-10">
-          {props.realtiveUsers.map((realtiveUser, index) => {
-            return (
-              <div
-                key={index}
-                className={
-                  realtiveUser.active === true
-                    ? "px-6 py-1 w-full border-l-2 border-secondary flex first:bg-compiler-base"
-                    : "px-6 py-1 w-full flex"
-                }
-              >
-                <div className="w-14 text-sm inline-block flex items-center">
-                  {realtiveUser.rank}.
-                </div>
-                <div className="px-2 w-full inline-block flex">
-                  <div
-                    className={
-                      realtiveUser.active === true
-                        ? "items-center p-0.5 rounded-full border border-secondary"
-                        : "items-center"
-                    }
-                  >
-                    <img
-                      src={realtiveUser.active === true ? avatar1 : avatar}
-                      className="w-9 h-9 rounded-full"
-                      alt={realtiveUser.name}
-                    />
-                  </div>
-                  <div
-                    className={
-                      realtiveUser.active === true
-                        ? "p-2 text-base font-normal inline-block align-middle font-semibold"
-                        : "p-2 text-base font-normal inline-block align-middle"
-                    }
-                  >
-                    {realtiveUser.name}
-                  </div>
-                </div>
-                <div className="pt-2">
-                  <div className="h-5 w-37 inline-block border border-seperator rounded-xl flex items-center justify-center">
-                    <span className="align-middle text-sm font-bold text-warning">
-                      {realtiveUser.score}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
       </div>
+      {/*</div>*/}
     </div>
   );
 };
--- app/src/lib/components/LeaderboardCard/LeaderboardCard.md
+++ app/src/lib/components/LeaderboardCard/LeaderboardCard.md
@@ -3,14 +3,72 @@ Leaderboard Card:
 ```js
 <LeaderboardCard
   topUsers={[
-    { rank: "1", name: "Marvin McKinney", score: 240 },
-    { rank: "2", name: "Albert Flores", score: 240 },
-    { rank: "3", name: "Annette Black", score: 240 },
+    {
+      leaderboardUser: {
+        fullName: "Marvin McKinney",
+        userId: 786,
+        profilePic: "",
+      },
+      rank: 1,
+      score: 1140,
+    },
+    {
+      leaderboardUser: {
+        fullName: "Albert Flores",
+        userId: 787,
+        profilePic: "",
+      },
+      rank: 2,
+      score: 1140,
+    },
+    {
+      leaderboardUser: {
+        fullName: "Annette Black",
+        userId: 788,
+        profilePic: "",
+      },
+      rank: 3,
+      score: 1140,
+    },
   ]}
   realtiveUsers={[
-    { rank: "19.7k", name: "Bishal Mishra", score: 0, active: true },
-    { rank: "19.7k", name: "Annette Black", score: 0 },
-    { rank: "19.7k", name: "Darlene Robertson", score: 0 },
+    {
+      leaderboardUser: {
+        fullName: "Bishal Mishra",
+        userId: 10,
+        profilePic: "",
+      },
+      rank: "19.7k",
+      score: 0,
+      loggedInId: 10,
+    },
+    {
+      leaderboardUser: {
+        fullName: "Annette Black",
+        userId: 11,
+        profilePic: "",
+      },
+      rank: "19.7k",
+      score: 0,
+    },
+    {
+      leaderboardUser: {
+        fullName: "Darlene Robertson",
+        userId: 12,
+        profilePic: "",
+      },
+      rank: "19.7k",
+      score: 0,
+    },
+    {
+      leaderboardUser: {
+        fullName: "Darlene Robertson",
+        userId: 13,
+        profilePic: "",
+      },
+      rank: "19.7k",
+      score: 0,
+    },
   ]}
 />
 ```
