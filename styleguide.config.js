154a1d096249ab20518eb3ac7ea22405033f8321
Sayush Lal Shrestha <lalsayush@gmail.com>
2021-07-21 13:28:02 +0545

    remove tailwind

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,19 +1,7 @@
 /* styleguide.config.js */
 const path = require('path');
 
-const {
-  createWebpackDevConfig,
-  createWebpackProdConfig,
-} = require('@craco/craco');
-
-const cracoConfig = require('./craco.config.js');
-const webpackConfig =
-  process.env.NODE_ENV === 'production'
-    ? createWebpackProdConfig(cracoConfig)
-    : createWebpackDevConfig(cracoConfig);
-
 module.exports = {
-  webpackConfig,
   components: 'src/lib/components/**/*.js',
   getComponentPathLine(componentPath) {
     const name = path.basename(componentPath, '.js');
