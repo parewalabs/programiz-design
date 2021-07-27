1e0472b49c6038cc4c2894d1516759d462afb9d0
Prabesh Shakya <prabesh595@gmail.com>
2021-07-27 13:47:16 +0545

    Fix courseindex

--- src/lib/language/CourseCard.language.js
+++ src/lib/language/CourseCard.language.js
@@ -1,2 +1,5 @@
 export const COMPLETE = "Complete";
 export const START_LEARNING = "Start Learning";
+export const COURSE_INDEX = "Course Index";
+export const EXPAND_ALL = "Expand All";
+export const COLLAPSE_ALL = "Collapse All";
